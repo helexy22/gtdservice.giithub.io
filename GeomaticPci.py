e2d6d110c5f8d620d3cad1b557c21eb04324b5b0
Helexy22 <lj288_learning@163.com>
2019-03-21 10:41:04 +0800

    dataset

--- GeomaticPci.py
+++ GeomaticPci.py
@@ -1,11 +1,12 @@
-from pci.api import datasource
-
-## how to open the irvine.pix Dataset
+# how to open the irvine.pix Dataset
 import os
 import pci
 from pci.api import datasource as ds
-# get the path to irvine.pix in the demo folder
-irvine_pix = os.path.join(pci.getHomePath(), 'demo', 'irvine.pix')
+irvine_pix = os.path.join(pci.getHomePath(), 'demo', 'irvine.pix') # get the path to irvine.pix in the demo folder
+dataset = ds.open_dataset(irvine_pix) # open the dataset
 
-# open the dataset
-dataset = ds.open_dataset(irvine_pix)
+# The following example shows how get the number of rows, columns and channels of a Dataset.
+with ds.open_dataset(irvine_pix) as dataset:
+    rows = dataset.width
+    cols = dataset.height
+    chans = dataset.chan_count
