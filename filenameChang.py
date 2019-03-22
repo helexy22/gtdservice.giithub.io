fe41963359c9d05455c96d2fb9453c43dd0e19a6
Helexy22 <lj288_learning@163.com>
2019-03-22 15:47:09 +0800

    文件重命名和写入txt

--- filenameChang.py
+++ filenameChang.py
@@ -1,19 +1,113 @@
 import os
 import re
 
-def replaceDirName(rootDir)
-dirs = os.listdir(rootDir)
-for dir in dirs:
-    print('oldname is:'+dir)   #输出老的名字
-
-    string = dir.split('、')
-    num = string[0]
-    temp = "%03d"%int(num)+'_'+string[1]  #主要目的是再数字统一为3位，不够前面补0
+def replaceDirName(rootDir)  
+dirs = os.listdir(rootDir) 
+for dir in dirs:   # 每一次循环都会执行一次，可以保证每次的结果换行
+    print('oldname is:'+dir)   #注意是输出 dir ，不是dirs（文件夹下所有的文件包括子文件夹）。
+    string = dir.split('.')
+    num = string[0]  # 次级数组的第一项
+     temp = "%03d"%int(num)+'_'+string[1]  #主要目的是再数字统一为3位，不够前面补0
 
     oldname = os.path.join(rootDir,dir)  #老文件夹的名字
     newname = os.path.join(rootDir,temp) #新文件夹的名字
 
     os.rename(oldname,newname)
+
 if __name__=='_main_':
-    rootDir = F:\下载\光盘\Java入门配套\daima'
+    rootDir = F:/下载/光盘/Java入门配套/daima'
     replaceDirName(rootdir)
+
+# ##########
+# ##########
+#
+# # Python读取文件夹下的所有文件
+#
+import os
+path = "F:/" #文件夹目录
+files= os.listdir(path) #得到文件夹下的所有文件名称
+s = []
+for file in files: #遍历文件夹
+     if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
+          f = open(path+"/"+file); #打开文件
+          iter_f = iter(f); #创建迭代器
+          str = ""
+          for line in iter_f: #遍历文件，一行行遍历，读取文本
+              str = str + line
+          s.append(str) #每个文件的文本存到list中
+print(s) #
+
+import os
+path="F:/r_GFgd191/water"
+files= os.listdir(path) # 获取所有文件夹下文件的名称，结果是数组
+s=[]
+for file in files:
+    if not os.path.isdir(file)
+
+
+# -*- coding: utf-8 -*-   
+import time     
+import os  
+import shutil
+ 
+ 
+def readFilename(path, allfile):
+    filelist = os.listdir(path)
+ 
+    for filename in filelist:
+        filepath = os.path.join(path, filename)
+        if os.path.isdir(filepath): #filepath 不是否是文件夹
+            readFilename(filepath, allfile)
+        else:
+            allfile.append(filepath)
+    return allfile
+ 
+ 
+def mycopyfile(srcfile,dstfile):
+    if not os.path.isfile(srcfile):
+        print "%s not exist!"%(srcfile)
+    else:
+        fpath,fname=os.path.split(dstfile)    #分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)                #创建路径
+        shutil.copyfile(srcfile,dstfile)      #复制文件
+        print "copy %s -> %s"%( srcfile,dstfile)
+ 
+ 
+if __name__ == '__main__':
+    
+    path1="/Users/sunny/Desktop/123/"
+    path2="/Users/sunny/Desktop/456/"
+    path3="/Users/sunny/Desktop/789/"
+    allfile1=[]
+    allfile2=[]
+	allfile1=readFilename(path1,allfile1)
+    allfile2=readFilename(path2,allfile2)
+    allname1=[]
+    allname2=[]
+    for name in allfile1:
+        t=name.split("/")[-1][0:-4]
+        print t
+        allname1.append(t)
+        print allname1
+    for name in allfile2:
+        t=name.split("/")[-1][0:-9]
+        print t
+        allname2.append(t)
+        print allname2
+    s1=[]
+    for ff in allname1:
+        f (ff not in allname2):
+            s1.append(ff)
+    print s1
+    s2=[]
+    for ff in allname1:
+        if (ff in allname2):
+            s2.append(ff)
+    print s2
+    for ns in s2:
+        srcfile=path1+str(ns)+'.jpg'
+        dstfile=path3+str(ns)+'.jpg'
+        mycopyfile(srcfile,dstfile)
+
+
---
+++ rename_createText.py
@@ -0,0 +1,71 @@
+## 读取目录中的.txt文件
+## 功能重命名文件，新建txt,并且写入txt
+
+import os
+import io
+import sys
+#改变标准输出的默认编码
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+
+src_dir_path = 'F:/r_GFgd191/water/change/'   # 文件夹后面的/是必须的，因为/可以区分文件和文件夹  #输入文件的位置
+toSrc_dir_path = 'F:/r_GFgd191/water/change/new/'                                             #输出文件的位置
+
+### 判断输出文件夹是否存在，如果不存在就新建文件夹
+newfolder=os.path.exists(toSrc_dir_path)    
+if not newfolder:
+        os.makedirs(toSrc_dir_path)
+        print("creater !toSrc_dir_path!  success ")
+else:
+	print ('---  There is this folder!  ---')
+
+key = '.tif'                           #需要重命名文件的后缀
+newfiletype=".txt"                     #新增加文件的后缀
+
+dirs=os.listdir(src_dir_path)
+for dir in dirs:
+    if key in dir:   #只读取含key的文件
+        string1 = dir.split('.tif')
+        string2 = dir.split('_')
+        filename="GF1_"+string1[0]
+
+        ### tif文件重命名
+        temp=filename+".tif"    #新文件名必须带后缀
+        oldname = os.path.join(src_dir_path,dir) 
+        newname = os.path.join(src_dir_path,temp)
+        os.rename(oldname,newname)
+        print("rename success")
+
+        ### 判断水质参数的名称
+        watercalss=string2[4].split('.')
+        string2[4]=watercalss[0]
+        if string2[4]=='u':   #1
+                imagecalss="COD浓度图像"
+        elif string2[4]=='ucol':
+                imagecalss="COD浓度专题图"  
+        elif string2[4]=='al':
+                imagecalss="水华指数图像"  #2
+        elif string2[4]=='alcol':
+                imagecalss="水华指数专题图"
+        elif string2[4]=='s':
+                imagecalss="混浊度图像" #3
+        elif string2[4]=='scol':
+                imagecalss="混浊度专题图"  
+        elif string2[4]=='c':
+                imagecalss="叶绿素浓度图像" #4 
+        elif string2[4]=='ccol':
+                imagecalss="叶绿素浓度专题图"
+        elif string2[4]=='cls':   #5
+                imagecalss="水质综合等级图像"
+        elif string2[4]=='clscol':
+                imagecalss="水质综合等级专题图"
+        else:
+                imagecalss="其他"
+                print(string2[4]+"-----的文件名错误------！！！！！！，文件编号:"+string1[0])
+        # 优化方向将if语句转为一个数组，数组中的元素一一对应
+
+        ### 在设定目录下新建txt文件，并写入msg
+        msg="卫星：GF-1"'\n'+'传感器类型：'+string2[0] +'\n'+"成像时间："+string2[3]+'\n'+"产品类型："+ imagecalss+'\n'+"生产单位：中山大学"   #txt文件中输入的内容
+        ol=open(toSrc_dir_path+filename+newfiletype,'w')  
+        ol.write(msg)
+      
+print('运行成功')
